5ca5a4caeffb9a281d16016ea9b3533d5831fabe
vince <vince.ycy@gmail.com>
2018-05-24 10:48:09 +0800

    fix the budget calculator failed

--- app/actions/SearchBudget.js
+++ app/actions/SearchBudget.js
@@ -17,19 +17,19 @@ export function search(start, end){
   var dateFormat = 'YYYY-MM-DD';
   var dateNoDayFormat = 'YYYY-MM';
 
-  var startDate = parseDate(moment(start, dateFormat));
-  var endDate = parseDate(moment(end, dateFormat));
+  var startDate = moment(start, dateNoDayFormat);
+  var endDate = moment(end, dateNoDayFormat);
 
-  var startMonth = moment(start).format(dateNoDayFormat)
-  var endMonth = moment(end).format(dateNoDayFormat)
+  var startDay = parseDate(moment(start, dateFormat)).day;
+  var endDay = parseDate(moment(end, dateFormat)).day;
 
   var budgets = getBudgets();
 
   var items = [];
   var index = 0;
   budgets.forEach(budget => {
-    var date = moment(budget.month).format(dateFormat)
-    if(moment(date).isSameOrAfter(startMonth) && moment(date).isSameOrBefore(endMonth)) {
+    var date = moment(budget.month, dateFormat);
+    if(moment(date).isSameOrAfter(startDate) && moment(date).isSameOrBefore(endDate)) {
       items[index] = budget;
       index++;
     }
@@ -38,15 +38,14 @@ export function search(start, end){
   items.forEach(budget => {
     var date = moment(budget.month, dateNoDayFormat);
     var dayOfMonth = moment(budget.month).daysInMonth();
-    var month = parseDate(date).month;
 
     var range = dayOfMonth;
-    if(startMonth == endMonth) {
-      range = endDate.day - startDate.day + 1;
-    } else if(month == startDate.month) {
-      range = dayOfMonth - startDate.day + 1;
-    } else if(month == endDate.month) {
-      range = endDate.day;
+    if(moment(startDate).isSame(endDate)) {
+      range = endDay - startDay + 1;
+    } else if(moment(date).isSame(startDate)) {
+      range = dayOfMonth - startDay + 1;
+    } else if(moment(date).isSame(endDate)) {
+      range = endDay;
     } else {
       range = dayOfMonth;
     }
